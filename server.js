b9b4a8536f5e71df5269dba0e46688a46be04fec
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2024-11-07 13:02:13 -0800

    get rid of ded code, comment

--- server.js
+++ server.js
@@ -16,29 +16,6 @@ app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'public', 'index.html'));
 });
 
-// app.post('/auth',
-//     nostrAuth.middleware(),
-//     (req, res) => {
-//         try {
-//             // Accessing the JSON body sent by the client
-//             const { room, username, avatarURL, relays, isPresenter } = req.body;
-//             console.log('Room:', room);
-//             console.log('Username:', username);
-//             console.log('Avatar URL:', avatarURL);
-//             console.log('Relays:', relays);
-//             console.log('isPresenter', isPresenter);
-
-//             // TODO: Redirect to hivetalk room give above info, correctly
-//             res.status(200).json({ message: 'Authentication successful'});
-
-//         } catch (error) {
-//             console.log("authentication failed")
-//             res.status(401).json({ error: 'Authentication failed' });
-//         }
-//     }
-// );
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
